8a8e1d9816aee83e0d8e7ceceff9700c1802850a
hong <785131182@qq.com>
2019-04-18 16:57:43 +0800

    feature:缓存器

--- 8.go_buffer/buffer.go
+++ 8.go_buffer/buffer.go
@@ -1,8 +1,85 @@
 package __go_buffer
 
-/*
-    @Create by GoLand
-    @Author: hong
-    @Time: 2019-04-18 10:43 
-    @File: buffer.go    
-*/
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+type Buffer interface {
+	Cap() uint32                         //获取缓冲器容量
+	Len() uint32                         //获取缓冲器中的数据数量
+	Put(datum interface{}) (bool, error) //向缓冲器放入数据
+	Get() (interface{}, error)           //从缓冲器中获取数据
+	Close() bool                         //关闭缓冲器
+	Closed() bool                        //缓冲器是否已关闭
+}
+
+type myBuffer struct {
+	ch          chan interface{} //数据通道
+	closed      uint32           //缓冲器状态量：0-未关闭  1-已关闭
+	closingLock sync.RWMutex     //为了消除因关闭缓冲器而产生的竞态条件的读写锁
+}
+
+func NewBuffer(size uint32) (Buffer, error) {
+	if size == 0 {
+		return nil, errors.New(fmt.Sprintf("illegal size for buffer: %d", size))
+	}
+	return &myBuffer{
+		ch: make(chan interface{}, size),
+	}, nil
+}
+
+func (buf *myBuffer) Cap() uint32 {
+	return uint32(cap(buf.ch))
+}
+
+func (buf *myBuffer) Len() uint32 {
+	return uint32(len(buf.ch))
+}
+
+func (buf *myBuffer) Put(datum interface{}) (ok bool, err error) {
+	buf.closingLock.RLock()
+	defer buf.closingLock.RUnlock()
+	if buf.Closed() {
+		return false, errors.New("closed buffer")
+	}
+	select {
+	case buf.ch <- datum:
+		ok = true
+	default:
+		ok = false
+
+	}
+	return
+}
+
+func (buf *myBuffer) Get() (interface{}, error) {
+	select {
+	case datum, ok := <-buf.ch:
+		if !ok {
+			return nil, errors.New("closed buffer")
+		}
+		return datum, nil
+	default:
+		return nil, nil
+	}
+}
+
+func (buf *myBuffer) Close() bool {
+	if atomic.CompareAndSwapUint32(&buf.closed, 0, 1) {
+		buf.closingLock.Lock()
+		close(buf.ch)
+		buf.closingLock.Unlock()
+		return true
+	}
+	return false
+}
+
+func (buf *myBuffer) Closed() bool {
+	if atomic.LoadUint32(&buf.closed) == 0 {
+		return false
+	}
+	return true
+}
--- 8.go_buffer/pool.go
+++ 8.go_buffer/pool.go
@@ -1,8 +1,192 @@
 package __go_buffer
 
-/*
-    @Create by GoLand
-    @Author: hong
-    @Time: 2019-04-18 10:43 
-    @File: pool.go    
-*/
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+type Pool interface {
+	BufferCap() uint32                   //获取缓冲器的统一容量
+	MaxBufferNumber() uint32             //获取pool中缓冲器最大数量
+	BufferNumber() uint32                //获取pool中缓冲器的数量
+	Total() uint64                       //获取pool中数据的总数
+	Put(datum interface{}) error         //向池中放入数据，若pool已关闭则直接返回错误
+	Get() (datum interface{}, err error) //从pool中获取数据
+	Close() bool
+	Closed() bool
+}
+
+func NewPool(bufferCap uint32, maxBufferNumber uint32) (Pool, error) {
+	if bufferCap == 0 {
+		return nil, errors.New(fmt.Sprintf("illegal buffer cap for buffer pool: %d", bufferCap))
+	}
+	if maxBufferNumber == 0 {
+		return nil, errors.New(fmt.Sprintf("illegal max buffer number for buffer pool: %d", maxBufferNumber))
+	}
+	bufCh := make(chan Buffer, maxBufferNumber)
+	buf, _ := NewBuffer(bufferCap)
+	bufCh <- buf
+	return &myPool{
+		bufferCap:       bufferCap,
+		maxBufferNumber: maxBufferNumber,
+		bufferNumber:    1,
+		bufCh:           bufCh,
+	}, nil
+}
+
+type myPool struct {
+	bufferCap       uint32      //缓冲器统一容量
+	maxBufferNumber uint32      //缓冲器的最大数量
+	bufferNumber    uint32      //缓冲器实际数量
+	total           uint64      //数据的总和
+	bufCh           chan Buffer //存放缓冲器的通道
+	closed          uint32      //pool的状态: 0-未关闭 1-已关闭
+	rwlock          sync.RWMutex
+}
+
+func (pool *myPool) BufferCap() uint32 {
+	return pool.bufferCap
+}
+
+func (pool *myPool) MaxBufferNumber() uint32 {
+	return pool.maxBufferNumber
+}
+
+func (pool *myPool) BufferNumber() uint32 {
+	return atomic.LoadUint32(&pool.bufferNumber)
+}
+
+func (pool *myPool) Total() uint64 {
+	return atomic.LoadUint64(&pool.total)
+}
+
+func (pool *myPool) Put(datum interface{}) (err error) {
+	if pool.Closed() {
+		return errors.New("closed buffer pool")
+	}
+	var count uint32
+	maxCount := pool.BufferNumber() * 5
+	var ok bool
+	for buf := range pool.bufCh {
+		ok, err = pool.putData(buf, datum, &count, maxCount)
+		if ok || err != nil {
+			break
+		}
+	}
+	return
+}
+
+func (pool *myPool) putData(buf Buffer, datum interface{}, count *uint32, maxCount uint32) (ok bool, err error) {
+	if pool.Closed() {
+		return false, errors.New("closed buffer pool")
+	}
+
+	defer func() {
+		pool.rwlock.Lock()
+		if pool.Closed() {
+			atomic.AddUint32(&pool.bufferNumber, ^uint32(0))
+			err = errors.New("closed buffer pool")
+		} else {
+			pool.bufCh <- buf
+		}
+		pool.rwlock.RUnlock()
+	}()
+
+	ok, err = buf.Put(datum)
+	if ok {
+		atomic.AddUint64(&pool.total, 1)
+	}
+	if err != nil {
+		return
+	}
+	(*count)++
+	if *count >= maxCount && pool.BufferNumber() < pool.MaxBufferNumber() {
+		pool.rwlock.Lock()
+		if pool.BufferNumber() < pool.MaxBufferNumber() {
+			if pool.Closed() {
+				pool.rwlock.Unlock()
+				return
+			}
+			newBuf, _ := NewBuffer(pool.bufferCap)
+			newBuf.Put(datum)
+			pool.bufCh <- newBuf
+			atomic.AddUint32(&pool.bufferNumber, 1)
+			atomic.AddUint64(&pool.total, 1)
+			ok = true
+		}
+		pool.rwlock.Unlock()
+		*count = 0
+	}
+	return
+}
+
+func (pool *myPool) Get() (datum interface{}, err error) {
+	if pool.Closed() {
+		return nil, errors.New("closed buffer pool")
+	}
+	var count uint32
+	maxCount := pool.BufferNumber() * 10
+	for buf := range pool.bufCh {
+		datum, err = pool.getData(buf, &count, maxCount)
+		if datum != nil || err != nil {
+			break
+		}
+	}
+	return
+}
+
+func (pool *myPool) getData(buf Buffer, count *uint32, maxCount uint32) (datum interface{}, err error) {
+	if pool.Closed() {
+		return nil, errors.New("closed buffer pool")
+	}
+	defer func() {
+		if *count >= maxCount && buf.Len() == 0 && pool.BufferNumber() > 1 {
+			buf.Close()
+			atomic.AddUint32(&pool.bufferNumber, ^uint32(0))
+			*count = 0
+			return
+		}
+		pool.rwlock.RLock()
+		if pool.Closed() {
+			atomic.AddUint32(&pool.bufferNumber, ^uint32(0))
+			err = errors.New("closed buffer pool")
+		} else {
+			pool.bufCh <- buf
+		}
+		pool.rwlock.RUnlock()
+	}()
+
+	datum, err = buf.Get()
+	if datum != nil {
+		atomic.AddUint64(&pool.total, ^uint64(0))
+		return
+	}
+	if err != nil {
+		return
+	}
+	(*count)++
+	return
+}
+
+func (pool *myPool) Close() bool {
+	if !atomic.CompareAndSwapUint32(&pool.closed, 0, 1) {
+		return false
+	}
+	pool.rwlock.Lock()
+	defer pool.rwlock.Unlock()
+
+	close(pool.bufCh)
+	for buf := range pool.bufCh {
+		buf.Close()
+	}
+	return true
+}
+
+func (pool *myPool) Closed() bool {
+	if atomic.LoadUint32(&pool.closed) == 1 {
+		return true
+	}
+	return false
+}
